2d059cd05f849091126d3cc4ad8f8e3340869c13
Rohan Basnet <rohanbasnet@Rohans-MacBook-Air.local>
2024-05-10 09:43:52 +0545

    program in python of Computer Graphics of DDA

---
+++ DDA.py
@@ -0,0 +1,44 @@
+import pygame
+import sys
+#initialize pygame
+pygame.init()
+#setup the display
+WIDTH, HEIGHT=800,600
+screen=pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("DDA Line Drawing Algorithm")
+#colors
+WHITE=(255, 255, 255)
+BLACK=(0,0,0)
+#Function to draw a line using DDA algorithm
+def drawLineDDA(x1,y1,x2,y2):
+    dx=x2-x1
+    dy=y2-y1
+    steps=max(abs(dx), abs(dy))
+    xIncrement=dx/steps
+    yIncrement=dy/steps
+    x=x1
+    y=y1
+    for i in range(steps):
+        screen.set_at((round(x),round(y)),WHITE)
+        x+=xIncrement
+        y+=yIncrement
+
+
+#main loop
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        #clear the screen
+        screen.fill(BLACK)
+        #draw aline using DDA algorithm
+        drawLineDDA(20,20,100,100)
+        #update the diplay
+        pygame.display.flip()
+
+if __name__== "__main__":
+    main()
+
+
